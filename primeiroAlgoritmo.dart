c6ec258317fb55b4977077296100f240059fc9ef
Leandro Santos <leanddrosanttoss@gmail.com>
2021-07-29 15:24:13 -0300

    Primeiro algoritmo

---
+++ primeiroAlgoritmo.dart
@@ -0,0 +1,15 @@
+main() {
+  //perguntar a idade da pessoa;
+  //se a idade for maior ou igual a 18
+  //ele é maior de idade;
+  //se não for
+  //ele não é maior de idade;
+
+  var idade = 18;
+
+  if (idade >= 18) {
+    print("Maior de idade");
+  } else {
+    print("Menor de idade");
+  }
+}
